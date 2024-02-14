7793739729726e984ca1b5f34311d614987247d0
kimkyungho <dev_kimkyungho@kakao.com>
2024-02-14 22:31:29 +0900

    카카오 주소검색 api 구현 RestTemplate 환경설정 추가

---
+++ src/main/java/com/example/pharmacy/config/RestTemplateConfig.java
@@ -0,0 +1,18 @@
+package com.example.pharmacy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by KimKyungHo on 2024-02-14(014)
+ */
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
